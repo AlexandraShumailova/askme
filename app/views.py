b85b253174b56a9fe4906d16d9668bdc7b720250
AlexandraShumailova <shumailova.alexandra@yandex.ru>
2023-11-15 01:00:51 +0300

    hw2 base

--- app/views.py
+++ app/views.py
@@ -1,9 +1,69 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+TAGS = [
+        {
+            'id': i,
+            'title': f'Tag {i}',
+        } for i in range(100)
+    ]
+
+QUESTIONS = [
+        {
+            'id': i,
+            'title': f'Question {i}',
+            'content': f'Long lorem ipsum {i}',
+            'tags': TAGS[i]
+        } for i in range(40)
+    ]
+
+HOT_QUESTIONS = QUESTIONS [:-5]
+
+ANSWERS = [
+        {
+            'id': i,
+            'content': f'Content of this answer {i}',
+            #correct = true/false?
+        } for i in range(10)
+    ]
+
+def paginate(objects, page, per_page=10):
+    paginator = Paginator(objects, per_page)
+
+    #page_item = paginator.page(1).object_list
+
+    #возвращаем нужную страницу (со всей инфой)
+    return paginator.page(page)
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    page = request.GET.get('page', 1)
+    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page)})
 
-def question(request):
-    return render(request, 'question.html')
+def hot_questions(request):
+    page = request.GET.get('page', 1)
+    return render(request, 'hot_questions.html', {'hot_questions': paginate(HOT_QUESTIONS, page)})
+
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    page = request.GET.get('page', 1)
+    return render(request, 'question.html', {'question': item, 'answers': paginate(ANSWERS, page, 3)})
+
+def ask(request):
+    return render(request, 'ask.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def tag(request, tag_id):
+    item = TAGS[tag_id]
+    page = request.GET.get('page', 1)
+    #надо будет выбирать вопросы, которые соответствуют тэгу. сейчас просто буду водить все
+    return render(request, 'tag.html', {'tag': item, 'questions': paginate(QUESTIONS, page)})
--- askme/urls.py
+++ askme/urls.py
@@ -19,7 +19,14 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index),
-    path('question', views.question),
+    path('', views.index, name='index'),
+    path('hot/', views.hot_questions, name='hot_questions'),
+    path('question/<int:question_id>', views.question, name='question'),
+    path('ask/', views.ask, name='ask'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('settings/', views.settings, name='settings'),
+    path('tag/<int:tag_id>/', views.tag, name='tag'),
+    #path('tag/blablabla', views.tag, name='tag_bla'),
     path('admin/', admin.site.urls),
 ]
